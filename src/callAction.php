ff58cd251cc23a2ce068188aea08968ae6a14501
Khalyomede <khalyomede@gmail.com>
2020-09-18 21:48:18 +0200

    #3 added second optional parameter to callAction to pass parameters to the called action (which fixes #3)

--- src/callAction.php
+++ src/callAction.php
@@ -8,7 +8,8 @@ if (!function_exists("callAction")) {
     /**
      * Include the content of the file.
      *
-     * @param string $path The path to the file (can use dot syntax).
+     * @param string $path       The path to the file (can use dot syntax).
+     * @param array  $parameters An array of key-value pairs to pass to the called action.
      *
      * @throws InvalidArgumentException If the action file is not found.
      *
@@ -17,8 +18,8 @@ if (!function_exists("callAction")) {
      * @example
      * callAction("home.index");
      */
-    function callAction(string $path): void
+    function callAction(string $path, array $parameters = []): void
     {
-        Action::call($path);
+        Action::call($path, $parameters);
     }
 }
